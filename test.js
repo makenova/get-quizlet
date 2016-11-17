2fb0cb7599ee735881f4704256c45fbb32e193d0
makenova <themarchmyth@gmail.com>
2016-11-17 15:27:49 -0600

    Update tests
    Add passing test

--- test.js
+++ test.js
@@ -1,6 +1,20 @@
 import test from 'ava'
-import { parseQiz } from './'
+import { parseQuiz } from './'
 
-test("test", t => {
-  t.fail();
+const TITLE = 'Test Title'
+const QUESTION = 'Why is the sky blue?'
+const ANSWER = 'Rayleigh Scattering'
+
+const testHTML = `
+<h1>${TITLE}</h1>
+<div class="SetPageTerm-wordText">${QUESTION}</div>
+<div class="SetPageTerm-definition">${ANSWER}</div>
+`
+
+test("test", async t => {
+  let quizObj = await parseQuiz(testHTML)
+
+  t.is(quizObj.title, TITLE)
+  t.deepEqual(quizObj.questions, [QUESTION])
+  t.deepEqual(quizObj.answers, [ANSWER])
 })
